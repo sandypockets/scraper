d7fd0137d94637c5c41e348885803d5987df9ae1
sandypockets <zac@znacreative.com>
2021-04-13 23:47:04 -0400

    Delete server

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const PORT = '8080';
-
-const server = http.createServer((req, res) => {
-})
-
-server.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`)
-})
