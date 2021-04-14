6951193473b7a67f63bb677599cd5dbb9c3ac9ef
sandypockets <zac@znacreative.com>
2021-04-13 23:45:42 -0400

    Set up Cheerio for class targeting

--- scraper.js
+++ scraper.js
@@ -1,13 +1,16 @@
 const axios = require('axios').default;
+const cheerio = require('cheerio');
 
 const scrape = (url, beacon) => {
   axios.get(url)
     .then((res) => {
-      console.log(res.data);
+      const $ = cheerio.load(res.data);
+      const target = $(beacon).text();
+      console.log(target.trim());
     })
     .catch(function() {
       console.log("error")
     })
 }
 
-scrape(`https://google.com`);
+scrape(`https://www.apple.com/ca/mac/`, ".typography-buystrip-copy");
